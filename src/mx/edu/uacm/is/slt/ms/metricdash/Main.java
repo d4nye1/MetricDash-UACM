6eaa6e291a444a46ccb07ab7320d9222d41b7f2f
Ernesto López <91957919+d4nye1@users.noreply.github.com>
2026-03-03 23:29:25 -0600

    Add Main class with startup message

---
+++ src/mx/edu/uacm/is/slt/ms/metricdash/Main.java
@@ -0,0 +1,7 @@
+package mx.edu.uacm.is.slt.ms.metricdash;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MetricDash UACM - Sistema Iniciado");
+    }
+}
