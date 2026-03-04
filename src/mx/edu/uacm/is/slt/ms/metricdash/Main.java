8c7ffb915410d63ad2b823ba13ee00889bc33d5c
Ernesto López <91957919+d4nye1@users.noreply.github.com>
2026-03-04 00:19:02 -0600

    Delete src/mx/edu/uacm/is/slt/ms/metricdash directory

--- src/mx/edu/uacm/is/slt/ms/metricdash/Main.java
+++
@@ -1,7 +0,0 @@
-package mx.edu.uacm.is.slt.ms.metricdash;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("MetricDash UACM - Sistema Iniciado");
-    }
-}
